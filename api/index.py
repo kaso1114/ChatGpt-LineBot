03213f5b0ec03d3e5a72c02aa49cb42148210287
林庭弘 <kaso1114@lintingongdeAir.lan>
2023-03-05 09:43:45 +0800

    v0.9

--- api/index.py
+++ api/index.py
@@ -9,7 +9,7 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, Sender
 import os
 
-VERSION = 'v0.8'
+VERSION = 'v0.9'
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
@@ -70,7 +70,7 @@ def handle_message(event):
     if working_status:
         chatgpt.add_msg(event.message.text)
         gpt_msg = chatgpt.get_response()
-        text = gpt_msg + f"\n\nTokens: {chatgpt.last_total_tokens} ({chatgpt.last_prompt_tokens}+{chatgpt.last_completion_tokens})"
+        text = gpt_msg + f"\n\nSpend: NT${(chatgpt.last_total_tokens/1000)*0.002:.1f}"
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=text, sender=Sender(icon_url=icon_url))
